1787275faedbec997a0e892e08226fa1ca02d6d2
Jonathan Diehl <jonathan.diehl@gmail.com>
2014-09-08 19:07:44 +0200

    bugfix: scope must be cloned

--- recipes/default.js
+++ recipes/default.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var path = require('path'),
+  extend = require('util')._extend,
   async = require('async'),
   mkweb = require('../lib/mkweb');
 
@@ -11,12 +12,12 @@ mkweb.registerRecipe('default', function (input, options, callback) {
     var scope = {};
     if (options.scope) {
       if (typeof options.scope === 'string') {
-        scope = require(path.join(process.cwd(), 'scopes', options.scope));
+        scope = require(path.join(process.cwd(), options.scope));
       } else {
         scope = options.scope;
       }
     }
-    done(null, scope);
+    done(null, extend({}, scope));
   }
 
   function compile(theScope, done) {
@@ -27,7 +28,7 @@ mkweb.registerRecipe('default', function (input, options, callback) {
   function applyLayout(content, done) {
     if (options.layout) {
       scope.content = content;
-      mkweb.compile(path.join('layout', options.layout), scope, done);
+      mkweb.compile(options.layout, scope, done);
     } else {
       done(null, content);
     }
